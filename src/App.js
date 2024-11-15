f557b8baa7860e499d513b4cec3129e53aa25d5b
B1AI5E <blaisewalsh@hotmail.com>
2024-11-15 11:42:11 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Budgeting App </h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h2>Home</h2>
+      <h3>Footer</h3>
+      {/* sdasdasd */}
+      {/* Blaise */}
+      {/* Brian */}
+    </div>
+  );
+}
